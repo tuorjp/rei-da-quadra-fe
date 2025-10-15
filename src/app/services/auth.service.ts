0f48143d432e2cf5a957ad1c1da211ea01a3bdea
JP <jplmarotinho22@gmail.com>
2025-10-15 11:27:05 -0300

    authService criado

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,51 @@
+import { Injectable, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationControllerService, AuthenticationDTO, LoginResponseDTO } from '../api';
+import { Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private readonly TOKEN_KEY = 'TK';
+  isLoggedIn = signal<boolean>(false);
+
+  constructor(
+    private authApi: AuthenticationControllerService,
+    private router: Router
+  ) {
+    const token = this.getToken();
+    if (token) {
+      this.isLoggedIn.set(true);
+    }
+  }
+
+  login(credentials: AuthenticationDTO): Observable<LoginResponseDTO> {
+    console.log(credentials)
+    return this.authApi.login(credentials).pipe(
+      tap(response => {
+        if (response.token) {
+          console.log('RESPONSE', response)
+          this.saveToken(response.token);
+          this.isLoggedIn.set(true);
+          this.router.navigate(['/']); // Redireciona para a home após o login
+        }
+      })
+    );
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.TOKEN_KEY);
+    this.isLoggedIn.set(false);
+    // Navega para o login explicitamente no logout
+    this.router.navigate(['/login']);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.TOKEN_KEY);
+  }
+
+  private saveToken(token: string): void {
+    localStorage.setItem(this.TOKEN_KEY, token);
+  }
+}
